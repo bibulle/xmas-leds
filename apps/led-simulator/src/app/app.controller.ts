42c4f89e69c7b9e1c9f21407c7d88648eaa14e3d
bibulle <famille.martin@gmail.com>
2024-11-08 11:56:56 +0100

    feat: add some content to the led simulator

--- apps/led-simulator/src/app/app.controller.ts
+++ apps/led-simulator/src/app/app.controller.ts
@@ -1,75 +1,85 @@
 // leds.controller.ts
 import { Controller, Get, Post, Delete, Query, Body, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
+import { LedsStatus } from '@xmas-leds/api-interfaces';
 
 @Controller()
 export class LedsController {
   // Pour simuler les états des animations, des LEDs, etc.
-  private animations = [];
-  private ledStatus = { on: false, color: '#FFFFFF' };
+  private animations = [
+    { type: 'file', name: 'animations/Sparkle_0010.bin', size: '229060' },
+    { type: 'file', name: 'animations/program.csv', size: '214' },
+  ];
+  private ledStatus: LedsStatus = {
+    defined: true,
+    up: 772556,
+    heapSize: 323424,
+    heapFree: 227432,
+    heapMin: 223224,
+    heapMax: 110580,
+    totalBytes: 1441792,
+    usedBytes: 249856,
+    animOn: true,
+  };
 
   // Route /list pour lister les fichiers d'animations
   @Get('/list')
-  handleFileList(): string[] {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleFileList(@Query('dir') dir: string): any[] {
     return this.animations;
   }
 
   // Route /strip/clear pour effacer la bande de LEDs
   @Get('/strip/clear')
   handleStripClear(): string {
-    this.ledStatus = { on: false, color: '#000000' };
-    return 'LED strip cleared';
+    return ;
   }
 
   // Route /strip/set pour configurer une couleur spécifique
   @Get('/strip/set')
-  handleStripSet(@Query('color') color: string): string {
-    this.ledStatus.color = color || '#FFFFFF';
-    this.ledStatus.on = true;
-    return `LED strip set to color ${color}`;
+  handleStripSet(): string {
+    return ;
   }
 
   // Route /strip/change pour changer l'état de la bande de LEDs
   @Get('/strip/change')
   handleStripChange(): string {
-    this.ledStatus.on = !this.ledStatus.on;
-    return `LED strip turned ${this.ledStatus.on ? 'on' : 'off'}`;
+    return ;
   }
 
   // Route /getStatus pour obtenir l'état actuel de la bande de LEDs
   @Get('/getStatus')
   handleGetStatus() {
-    return {
-      status: this.ledStatus.on ? 'on' : 'off',
-      color: this.ledStatus.color,
-    };
+    return this.ledStatus;
   }
 
   // Route /anim/stop pour arrêter une animation
   @Get('/anim/stop')
   handleAnimStop(): string {
-    return 'Animation stopped';
+    this.ledStatus.animOn = false;
+    return ;
   }
 
   // Route /anim/start pour démarrer une animation
   @Get('/anim/start')
   handleAnimStart(): string {
-    return 'Animation started';
+    this.ledStatus.animOn = true;
+    return ;
   }
 
   // Route /upload pour télécharger un fichier d'animation (POST)
   @Post('/upload')
   handleFileUpload(): string {
-    return 'File uploaded';
+    return ;
   }
 
   // Route /anim pour supprimer un fichier d'animation (DELETE)
   @Delete('/anim')
   handleFileDelete(@Body('filename') filename: string): string {
-    const index = this.animations.indexOf(filename);
-    if (index > -1) {
-      this.animations.splice(index, 1);
-      return `File ${filename} deleted`;
-    }
+    // const index = this.animations.indexOf(filename);
+    // if (index > -1) {
+    //   this.animations.splice(index, 1);
+    //   return `File ${filename} deleted`;
+    // }
     throw new HttpException('File not found', HttpStatus.NOT_FOUND);
   }
 
@@ -89,11 +99,11 @@ export class LedsController {
   // Route /anim/rename pour renommer un fichier d'animation
   @Get('/anim/rename')
   handleRenameAnim(@Query('oldName') oldName: string, @Query('newName') newName: string): string {
-    const index = this.animations.indexOf(oldName);
-    if (index > -1) {
-      this.animations[index] = newName;
-      return `Animation ${oldName} renamed to ${newName}`;
-    }
+    // const index = this.animations.indexOf(oldName);
+    // if (index > -1) {
+    //   this.animations[index] = newName;
+    //   return `Animation ${oldName} renamed to ${newName}`;
+    // }
     throw new HttpException('File not found', HttpStatus.NOT_FOUND);
   }
 }
